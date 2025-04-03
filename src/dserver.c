ad964d44163a72b2176d9fd05c6def648ceab492
DIOGO4810 <ribeiroeribeiro2005@gmail.com>
2025-04-03 23:04:01 +0100

    Implementação de multiUser

--- src/dserver.c
+++ src/dserver.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <signal.h>
 #include <glib.h>
 #include "parser.h"
 
@@ -37,7 +38,8 @@ void freeEstrutura(GArray *estrutura)
 {
     for (guint i = 0; i < estrutura->len; i++)
     {
-        if(g_array_index(estrutura, Index *, i) == NULL)continue;
+        if (g_array_index(estrutura, Index *, i) == NULL)
+            continue;
         Index *item = g_array_index(estrutura, Index *, i);
         freeIndex(item);
     }
@@ -55,12 +57,13 @@ void printGArrayIndex(GArray *array)
     int numeroValidos = 0;
     for (guint i = 0; i < array->len; i++)
     {
-        if(g_array_index(array, Index *, i) == NULL)continue;
+        if (g_array_index(array, Index *, i) == NULL)
+            continue;
         numeroValidos++;
         Index *item = g_array_index(array, Index *, i);
         printIndice(item);
     }
-    printf("GArray contém %d elementos\n",numeroValidos);
+    printf("GArray contém %d elementos\n", numeroValidos);
 }
 
 void respondMessageAdiciona(char *diretoria, guint arraySize)
@@ -69,18 +72,18 @@ void respondMessageAdiciona(char *diretoria, guint arraySize)
     pid_t fdmessage = open(diretoria, O_WRONLY);
     char *message = malloc(256 * sizeof(char));
     sprintf(message, "Document %d indexed", (int)arraySize - 1);
-    (void) write(fdmessage, message, strlen(message));
+    (void)write(fdmessage, message, strlen(message));
     close(fdmessage);
     free(message);
 }
 
-void respondMessageConsulta(char *diretoria,Index* indice)
+void respondMessageConsulta(char *diretoria, Index *indice)
 {
     mkfifo(diretoria, 0666);
     pid_t fdmessage = open(diretoria, O_WRONLY);
     char *message = malloc(256 * sizeof(char));
-    sprintf(message, "%s %s %d %s",indice->title, indice->author, indice->year, indice->path);
-    (void) write(fdmessage, message, strlen(message));
+    sprintf(message, "%s %s %d %s", indice->title, indice->author, indice->year, indice->path);
+    (void)write(fdmessage, message, strlen(message));
     close(fdmessage);
     free(message);
 }
@@ -91,71 +94,105 @@ void respondErrorMessage(char *diretoria)
     pid_t fdmessage = open(diretoria, O_WRONLY);
     char *message = malloc(256 * sizeof(char));
     sprintf(message, "404");
-    (void) write(fdmessage, message, strlen(message));
+    (void)write(fdmessage, message, strlen(message));
     close(fdmessage);
     free(message);
 }
 
-void respondMessageRemove(char* diretoria , int indice){
+void respondMessageRemove(char *diretoria, int indice)
+{
     mkfifo(diretoria, 0666);
     pid_t fdmessage = open(diretoria, O_WRONLY);
     char *message = malloc(256 * sizeof(char));
-    sprintf(message, "Index entry %d deleted",indice);
-    (void) write(fdmessage, message, strlen(message));
+    sprintf(message, "Index entry %d deleted", indice);
+    (void)write(fdmessage, message, strlen(message));
     close(fdmessage);
     free(message);
-
 }
 
-
-
-void handleInput(char **tokens,GArray* indexArray)
+void handleInput(char **tokens, GArray *indexArray)
 {
-    
+    char diretoria[256] = "";
 
     switch (tokens[0][1])
     {
-    case 'a':{
+    case 'a':
+    {
 
         Index *indice = createIndex(tokens + 1);
         g_array_append_val(indexArray, indice);
-        respondMessageAdiciona("tmp/writeServerFIFO", indexArray->len);
+        sprintf(diretoria, "tmp/writeServerFIFO%d", atoi(tokens[5]));
+        respondMessageAdiciona(diretoria, indexArray->len);
     }
     break;
 
-    case 'c':{
+    case 'c':
+    {
         int indiceArray = atoi(tokens[1]);
-        if(indexArray->len == 0 || indiceArray < 0 || indiceArray > (int)indexArray->len ){
-            respondErrorMessage("tmp/writeServerFIFO");
+        sprintf(diretoria, "tmp/writeServerFIFO%d", atoi(tokens[2]));
+
+        if (indexArray->len == 0 || indiceArray < 0 || indiceArray > (int)indexArray->len)
+        {
+            respondErrorMessage(diretoria);
             break;
         }
-        if( g_array_index(indexArray,Index*, indiceArray) == NULL){
-            respondErrorMessage("tmp/writeServerFIFO");
+        if (g_array_index(indexArray, Index *, indiceArray) == NULL)
+        {
+            respondErrorMessage(diretoria);
             break;
         }
-        Index* indice = g_array_index(indexArray,Index*, indiceArray);
-        respondMessageConsulta("tmp/writeServerFIFO",indice);
+        Index *indice = g_array_index(indexArray, Index *, indiceArray);
+        respondMessageConsulta(diretoria, indice);
     }
     break;
 
-    case 'd':{
+    case 'd':
+    {
         int indiceArray = atoi(tokens[1]);
-        if(indiceArray < 0 || indiceArray > (int)indexArray->len){
-            respondErrorMessage("tmp/writeServerFIFO");
+        sprintf(diretoria, "tmp/writeServerFIFO%d", atoi(tokens[2]));
+
+        if (indiceArray < 0 || indiceArray > (int)indexArray->len)
+        {
+            respondErrorMessage(diretoria);
             break;
         }
-        freeIndex(g_array_index(indexArray,Index*, indiceArray));
-        g_array_index(indexArray,Index*, indiceArray) = NULL;
-        respondMessageRemove("tmp/writeServerFIFO",indiceArray);
-    }   
+        freeIndex(g_array_index(indexArray, Index *, indiceArray));
+        g_array_index(indexArray, Index *, indiceArray) = NULL;
+        respondMessageRemove(diretoria, indiceArray);
+    }
     break;
+    // case 'l':
+    // {
+    //     int indiceArray = atoi(tokens[1]);
+    //     sprintf(diretoria, "tmp/writeServerFIFO%d", atoi(tokens[2]));
+
+    //     if (indiceArray < 0 || indiceArray > (int)indexArray->len)
+    //     {
+    //         respondErrorMessage(diretoria);
+    //         break;
+    //     }
+    //     Index* indice = g_array_index(indexArray, Index *, indiceArray);
+    // }
+    // break;
     default:
         break;
     }
 }
 
+int checkAsync(char *input)
+{
+    if (input[1] == 'a' || input[1] == 'd' || input[1] == 'f')
+        return 0;
+    return 1;
+}
+
 int main()
 {
+    // struct sigaction sa;
+    // sa.sa_handler = SIG_IGN;  
+    // sigaction(SIGCHLD, &sa, NULL);
+
+    
 
     GArray *indexArray = g_array_new(FALSE, FALSE, sizeof(Index *));
     while (1)
@@ -170,6 +207,25 @@ int main()
         read(fd, clientInput, 512);
         printf("O cliente mandou isto:%s\n", clientInput);
 
+        int isAsync = checkAsync(clientInput);
+        if (isAsync)
+        {
+            pid_t pid;
+            if ((pid = fork()) == 0)
+            {
+                Parser *parseFIFO = newParser();
+                parseFIFO = parser(parseFIFO, clientInput);
+                char **tokens = getTokens(parseFIFO);
+
+                handleInput(tokens, indexArray);
+
+                freeParser(parseFIFO);
+                exit(0);
+            }
+            else
+                continue;
+        }
+
         Parser *parseFIFO = newParser();
         parseFIFO = parser(parseFIFO, clientInput);
         char **tokens = getTokens(parseFIFO);
@@ -181,7 +237,7 @@ int main()
             break;
         }
 
-        handleInput(tokens,indexArray);
+        handleInput(tokens, indexArray);
 
         freeParser(parseFIFO);
         close(fd);
